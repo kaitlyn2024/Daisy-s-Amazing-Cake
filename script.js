f56190150435754f3299d11f8c0f3a1d7906889d
kaitlyn2024 <71143346+kaitlyn2024@users.noreply.github.com>
2020-10-06 14:57:27 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 //scroll effect
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
 
-//logo animate
+    if(height  > 500) {
+        $(".fixed-top").css({ opacity: 0.8 });
+
+    } else{
+		$(".fixed-top").css({ opacity: 1 });
+	}
+});
+
+//fade in
+$(document).ready(function(){
+	$("#welcome-message").css({ opacity: 1 });
+});
